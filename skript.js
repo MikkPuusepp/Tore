89b1d435e5a63ac7b4e2c4912364767ef93997ca
MikkPuusepp <mikkpuusepp@gmail.com>
2022-02-08 15:58:05 +0200

    Vue  äppi mountimine
    linnulaul

---
+++ skript.js
@@ -0,0 +1,8 @@
+let app = Vue.createApp({
+    data(){
+        return{
+            linnulaul: `Hüüd mahe "gjük!", vahetevahel vaikne "kiuk!" või "blipp", ärevushüüd "gigrägra..."`
+        }
+    },
+})
+
